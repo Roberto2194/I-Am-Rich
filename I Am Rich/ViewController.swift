52d51d1b967877ed462c9a241c6ae85447f226fb
Roberto Liccardo <roberto_liccardo@outlook.it>
2020-03-13 17:34:59 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Roberto Liccardo on 13/03/2020.
+//  Copyright © 2020 Roberto Liccardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
